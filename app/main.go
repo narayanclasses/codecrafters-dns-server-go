5381beb71f32c1596d0639d9abbdccc0c559f606
nikhilk <nikhilk@arista.com>
2024-05-18 21:16:08 +0530

    test

--- app/main.go
+++ app/main.go
@@ -17,29 +17,34 @@ func main() {
 		fmt.Println("Failed to resolve UDP address:", err)
 		return
 	}
-	
+
 	udpConn, err := net.ListenUDP("udp", udpAddr)
 	if err != nil {
 		fmt.Println("Failed to bind to address:", err)
 		return
 	}
 	defer udpConn.Close()
-	
+
 	buf := make([]byte, 512)
-	
+
 	for {
 		size, source, err := udpConn.ReadFromUDP(buf)
 		if err != nil {
 			fmt.Println("Error receiving data:", err)
 			break
 		}
-	
+
 		receivedData := string(buf[:size])
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
-	
+
 		// Create an empty response
-		response := []byte{}
-	
+		response := []byte{
+			4, 210,
+			1,
+			0,
+			0, 0, 0, 0, 0, 0, 0, 0,
+		}
+
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
