7e2c9b15a3ce2069b327c61c28ace3a93978fba2
nikhilk <nikhilk@arista.com>
2024-05-18 22:49:57 +0530

    test

--- app/main.go
+++ app/main.go
@@ -1,13 +1,51 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
-	"strings"
-
-	// Uncomment this block to pass the first stage
+	"log"
 	"net"
+	"strings"
 )
 
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+	udpAddr, err := net.ResolveUDPAddr("udp", "localhost:2053")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer udpConn.Close()
+	buf := make([]byte, 512)
+	for {
+		size, source, err := udpConn.ReadFromUDP(buf)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		message := string(buf[:size])
+		fmt.Println(message)
+		fmt.Println(buf[:2])
+		response := []byte{}
+		response = append(response, buf[:2]...)
+		response = append(response, 0b10000000)
+		response = append(response, 0b00000000)
+		response = append(response, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)
+		response = binary.BigEndian.AppendUint16(response, uint16(1)) // QDCOUNT
+		response = binary.BigEndian.AppendUint16(response, 0x0000)    // ANCOUNT
+		response = binary.BigEndian.AppendUint16(response, 0x0000)    // NSCOUNT
+		response = binary.BigEndian.AppendUint16(response, 0x0000)    // ARCOUNT
+		response = append(response, encodeDomain("codecrafters.io")...)
+		response = binary.BigEndian.AppendUint16(response, uint16(1))
+		response = binary.BigEndian.AppendUint16(response, uint16(1))
+		if _, err := udpConn.WriteToUDP(response, source); err != nil {
+			log.Fatal(err.Error())
+		}
+	}
+}
 func encodeDomain(domain string) []byte {
 	encodes := []byte{}
 	for _, seg := range strings.Split(domain, ".") {
@@ -17,76 +55,7 @@ func encodeDomain(domain string) []byte {
 	}
 	return append(encodes, 0x00)
 }
-
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
-	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
-	if err != nil {
-		fmt.Println("Failed to resolve UDP address:", err)
-		return
-	}
-
-	udpConn, err := net.ListenUDP("udp", udpAddr)
-	if err != nil {
-		fmt.Println("Failed to bind to address:", err)
-		return
-	}
-	defer udpConn.Close()
-
-	buf := make([]byte, 512)
-
-	for {
-		_, source, err := udpConn.ReadFromUDP(buf)
-		if err != nil {
-			fmt.Println("Error receiving data:", err)
-			break
-		}
-
-		// Process received question
-		receivedQuestion := []byte{}
-		i := 12
-		for i < len(buf) {
-			fmt.Println("nikhilk2")
-			fmt.Println(buf[i])
-			length := int(buf[i])
-			fmt.Println(length)
-			receivedQuestion = append(receivedQuestion, buf[i])
-			if length == 0 {
-				break
-			}
-			i++ // move to the start of the segment
-			receivedQuestion = append(receivedQuestion, buf[i:i+length]...)
-			i += length // move to the next length prefix
-		}
-
-		receivedQuestion = append(receivedQuestion, 0, 1, 0, 1)
-
-		response := []byte{}
-		response = append(response,
-			4, 210,
-			128,
-			0,
-			1, 0,
-			0, 0,
-			0, 0,
-			0, 0)
-
-		fmt.Println(len(receivedQuestion))
-
-		response = append(response, receivedQuestion...)
-
-		fmt.Println(len(response))
-
-		fmt.Println("nikhilk3")
-		fmt.Println(response)
-
-		_, err = udpConn.WriteToUDP(response, source)
-		if err != nil {
-			fmt.Println("Failed to send response:", err)
-		}
-	}
+func bigendEncode(v int) []byte {
+	encodes := []byte{}
+	return binary.BigEndian.AppendUint16(encodes, uint16(v))
 }
