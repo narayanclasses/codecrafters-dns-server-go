9ceee56db0b7fc3fa1eca7b6de697185f7dcd9c0
nikhilk <nikhilk@arista.com>
2024-05-18 22:36:24 +0530

    test

--- app/main.go
+++ app/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"strings"
 
@@ -47,61 +46,39 @@ func main() {
 			break
 		}
 
-		// receivedBytes := []byte(string(buf[:size]))
-
-		// i := 0
-		// for i < len(receivedBytes) {
-		// 	fmt.Println("nikhilk")
-		// 	fmt.Println(receivedBytes[i])
-		// 	i++
-		// }
-
 		// Process received question
 		receivedQuestion := []byte{}
-		// i := 12
-		// for i < len(receivedBytes) {
-		// 	// fmt.Println("nikhilk2")
-		// 	// fmt.Println(receivedBytes[i])
-		// 	length := int(receivedBytes[i])
-		// 	// fmt.Println(length)
-		// 	receivedQuestion = append(receivedQuestion, receivedBytes[i])
-		// 	if length == 0 {
-		// 		break
-		// 	}
-		// 	i++ // move to the start of the segment
-		// 	receivedQuestion = append(receivedQuestion, receivedBytes[i:i+length]...)
-		// 	i += length // move to the next length prefix
-		// }
-
-		receivedQuestion = append(receivedQuestion, encodeDomain("codecrafters.io")...)
+		i := 14
+		for i < len(buf) {
+			// fmt.Println("nikhilk2")
+			// fmt.Println(buf[i])
+			length := int(buf[i])
+			// fmt.Println(length)
+			receivedQuestion = append(receivedQuestion, buf[i])
+			if length == 0 {
+				break
+			}
+			i++ // move to the start of the segment
+			receivedQuestion = append(receivedQuestion, buf[i:i+length]...)
+			i += length // move to the next length prefix
+		}
 
 		receivedQuestion = append(receivedQuestion, 0, 1, 0, 1)
 
-		// response := []byte{
-		// 	4, 210,
-		// 	128,
-		// 	0,
-		// 	1, 0,
-		// 	0, 0,
-		// 	0, 0,
-		// 	0, 0,
-		// }
-
 		response := []byte{}
 		response = append(response, buf[:2]...)
-		response = append(response, 0b10000000)
-		response = append(response, 0b00000000)
-		response = binary.BigEndian.AppendUint16(response, uint16(1)) // QDCOUNT
-		response = binary.BigEndian.AppendUint16(response, 0x0000)    // ANCOUNT
-		response = binary.BigEndian.AppendUint16(response, 0x0000)    // NSCOUNT
-		response = binary.BigEndian.AppendUint16(response, 0x0000)    // ARCOUNT
-		response = append(response, encodeDomain("codecrafters.io")...)
-		response = binary.BigEndian.AppendUint16(response, uint16(1))
-		response = binary.BigEndian.AppendUint16(response, uint16(1))
+		response = append(response,
+			4, 210,
+			128,
+			0,
+			1, 0,
+			0, 0,
+			0, 0,
+			0, 0)
 
-		// fmt.Println(len(receivedQuestion))
+		fmt.Println(len(receivedQuestion))
 
-		// response = append(response, receivedQuestion...)
+		response = append(response, receivedQuestion...)
 
 		fmt.Println(len(response))
 
